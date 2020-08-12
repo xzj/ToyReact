34c159eb11c6e93d67ecd3b6571f02a6225fbabe
xzj <xzj007@gmail.com>
2020-08-12 23:40:14 +0800

    First try to add reconciliation for ToyReact.

--- ToyReact.js
+++ ToyReact.js
@@ -4,6 +4,9 @@ export class Component {
         this.children = [];
         this.props = Object.create(null);
     }
+    get type() {
+        return this.constructor.name;
+    }
     setAttribute(name, v) {
         this.props[name] = v;
         this[name] = v;
@@ -47,15 +50,48 @@ export class Component {
         this.update();
     }
     update() {
-        const placeholder = document.createComment("placeholder");
-        const range = document.createRange();
-        range.setStart(this.range.endContainer, this.range.endOffset);
-        range.setEnd(this.range.endContainer, this.range.endOffset);
-        range.insertNode(placeholder);
-
-        this.range.deleteContents();
         const vdom = this.render();
-        vdom.mountTo(this.range);
+        if (this.vdom) {
+            const isSameNode = (node1, node2) => {
+                if (node1.type !== node2.type) {
+                    return false;
+                }
+                for (let name of node1.props) {
+                    if (node1.props[name] !== node2.props[name]) {
+                        return false;
+                    }
+                }
+                if (Object.keys(node1.props).length !== Object.keys(node2.props).length) {
+                    return false;
+                }
+                return true;
+            }
+            const isSameTree = (node1, node2) => {
+                if (!isSameNode(node1, node2)) {
+                    return false;
+                }
+                if (node1.children.length !=== node2.children.length) {
+                    return fasle;
+                }
+                for (let i = 0; i < node1.children.length; ++i) {
+                    if (!isSameNode(node1.children[i], node2.children[i])) {
+                        return false;
+                    }
+                }
+                return true;
+            }
+            if (isSameTree(vdom, this.vdom)) {
+                return;
+            }
+
+            console.log("new: ", vdom);
+            console.log("old: ", this.vdom);
+        }
+        else {
+            vdom.mountTo(this.range);
+        }
+
+        this.vdom = vdom;
     }
 }
 
@@ -95,6 +131,80 @@ class ElementWrapper {
     }
 }
 
+class TextWrapper {
+    constructor(content) {
+        this.root = document.createTextNode(content);
+        this.type = "#text";
+        this.props = Object.create(null);
+        this.children = [];
+    }
+    mountTo(range) {
+        range.deleteContents();
+        range.insertNode(this.root);
+        // parent.appendChild(this.root);
+    }
+}
+
+// export class Component {
+//     constructor() {
+//         this.children = [];
+//         this.props = Object.create(null);
+//     }
+//     setAttribute(name, v) {
+//         this.props[name] = v;
+//         this[name] = v;
+//     }
+//     appendChild(vchild) {
+//         // const vdom = this.render();
+//         // vdom.appendChild(vchild);
+//         this.children.push(vchild);
+//     }
+//     setState(state) {
+//         const merge = (oldState, newState) => {
+//             for (let p in newState) {
+//                 if ((typeof newState[p] === 'object') && (newState[p] !== null)) {
+//                     if (typeof oldState[p] !== 'object') {
+//                         if (newState[p] instanceof Array) {
+//                             oldState[p] = [];
+//                         }
+//                         else {
+//                             oldState[p] = {};
+//                         }
+//                     }
+//                     merge(oldState[p], newState[p]);
+//                 }
+//                 else {
+//                     oldState[p] = newState[p];
+//                 }
+//             }
+//         }
+//         if (!this.state && state) {
+//             this.state = {};
+//         }
+//         merge(this.state, state);
+//         this.update();
+//     }
+//     mountTo(range) {
+//         console.log("==== this: ", this, " mount to: ", range);
+//         // range.deleteContents();
+//         // const vdom = this.render();
+//         // vdom.mountTo(range);
+//         this.range = range;
+//         this.update();
+//     }
+//     update() {
+//         const placeholder = document.createComment("placeholder");
+//         const range = document.createRange();
+//         range.setStart(this.range.endContainer, this.range.endOffset);
+//         range.setEnd(this.range.endContainer, this.range.endOffset);
+//         range.insertNode(placeholder);
+
+//         this.range.deleteContents();
+//         const vdom = this.render();
+//         vdom.mountTo(this.range);
+//     }
+// }
+
 // class ElementWrapper {
 //     constructor(tag) {
 //         this.root = document.createElement(tag);
@@ -121,16 +231,16 @@ class ElementWrapper {
 //         // parent.appendChild(this.root);
 //     }
 // }
-class TextWrapper {
-    constructor(content) {
-        this.root = document.createTextNode(content);
-    }
-    mountTo(range) {
-        range.deleteContents();
-        range.insertNode(this.root);
-        // parent.appendChild(this.root);
-    }
-}
+// class TextWrapper {
+//     constructor(content) {
+//         this.root = document.createTextNode(content);
+//     }
+//     mountTo(range) {
+//         range.deleteContents();
+//         range.insertNode(this.root);
+//         // parent.appendChild(this.root);
+//     }
+// }
 
 export let ToyReact = {
     createElement(type, attributes, ...children) {
